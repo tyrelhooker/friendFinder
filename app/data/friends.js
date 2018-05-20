b759dfd61797f6d48c1584dd14c01bd71683a57f
T.J. Hooker <tyrelhooker@gmail.com>
2018-05-20 13:48:12 -0400

    added data values to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,72 +1,172 @@
 var friendsArr = [
   {
-    "name": "",
-    "photo": "",
+    "name": "Chihuahua",
+    "photo": "https://images.pexels.com/photos/191353/pexels-photo-191353.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     "scores": [
-
-    ]
+      1,
+      4,
+      5,
+      3,
+      4,
+      5,
+      5,
+      1,
+      1,
+      3
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Boxer",
+    "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/boxer-dogs-and-puppies/boxer-dogs-puppies-4.jpg",
     "scores": [
-
-    ]
+      2,
+      3,
+      5,
+      2,
+      4,
+      3,
+      4,
+      3,
+      1,
+      4
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Blue Heeler",
+    "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/australian-cattle-dog-dog-breed-pictures/7-sidestanding.jpg",
     "scores": [
-
-    ]
+      5,
+      2,
+      2,
+      1,
+      5,
+      5,
+      3,
+      4,
+      4,
+      5
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Cavalier King Charles Spaniel",
+    "photo": "http://cdn1-www.dogtime.com/assets/uploads/gallery/cavalier-king-charles-spaniel-dog-breed-pictures/9-backsitting.jpg",
     "scores": [
-
-    ]
+      3,
+      3,
+      3,
+      5,
+      3,
+      2,
+      5,
+      5,
+      1,
+      4
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Great Dane",
+    "photo": "http://cdn2-www.dogtime.com/assets/uploads/gallery/great-dane-dogs-and-puppies/great-dane-dogs-puppies-2.jpg",
     "scores": [
-
-    ]
+      1,
+      3,
+      1,
+      5,
+      2,
+      1,
+      5,
+      5,
+      1,
+      3
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "English Bulldog",
+    "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/bulldog-dog-breed-pictures/1-threequartersitting.jpg",
     "scores": [
-
-    ]
+      1,
+      4,
+      3,
+      5,
+      4,
+      1,
+      3,
+      2,
+      3,
+      2
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Springer Spaniel",
+    "photo": "http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg",
     "scores": [
-
-    ]
+      4,
+      2,
+      5,
+      1,
+      5,
+      4,
+      5,
+      5,
+      1,
+      5
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Weimaraner",
+    "photo": "http://cdn1-www.dogtime.com/assets/uploads/gallery/weimaraner-dog-breed-pictures/9-stand.jpg",
     "scores": [
-
-    ]
+      5,
+      2,
+      2,
+      1,
+      4,
+      3,
+      4,
+      3,
+      1,
+      5
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "Labrador Retriever",
+    "photo": "http://cdn2-www.dogtime.com/assets/uploads/gallery/labrador-retriever-dog-breed-pictures/labrador-retriever-dog-pictures-6.jpg",
     "scores": [
-
-    ]
+      4,
+      2,
+      3,
+      2,
+      5,
+      2,
+      5,
+      5,
+      2,
+      5
+    ],
+    "dog": true
   },
   {
-    "name": "",
-    "photo": "",
+    "name": "French Bulldog",
+    "photo": "http://cdn1-www.dogtime.com/assets/uploads/gallery/french-bulldog-dog-breed-pictures/1-puppy.jpg",
     "scores": [
-
-    ]
+      1,
+      3,
+      5,
+      4,
+      5,
+      4,
+      4,
+      4,
+      1,
+      3
+    ],
+    "dog": true
   }
 ];
