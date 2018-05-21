1334a4fa745529600164e48b010afff1d9d25c93
T.J. Hooker <tyrelhooker@gmail.com>
2018-05-20 22:23:00 -0400

    working submit button in html and convert survey results into array in apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,15 @@ module.exports = function (app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
+
+  app.post("/api/friends", function(req, res) {
+    console.log("New Human: " + JSON.stringify(req.body));
+    var humanScores = req.body.scores;
+    var humanScoresArr = [];
+    for (var i = 0; i < humanScores.length; i++) {
+      humanScoresArr.push(humanScores[i]);
+    }
+
+    console.log("humanScoresArr: " + humanScoresArr);
+  });
 }
